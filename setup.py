e1229d630305992943e9d3f72fb9455a069badfd
Alexey Pechenkin <alexey74@gmail.com>
2021-12-16 07:27:54 -0500

    released setuptools version pin

--- setup.py
+++ setup.py
@@ -5,5 +5,5 @@ Package config script for 3D bin packing library.
 import setuptools  # noqa
 
 setuptools.setup(
-    setup_requires="setuptools==46.4.0",
+    setup_requires="setuptools>=46.4.0",
 )
