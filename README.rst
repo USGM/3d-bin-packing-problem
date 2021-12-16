48edd85fbe8a8f9135f37aec87f3794b2a86566d
Alexey Pechenkin <alexey74@gmail.com>
2021-12-16 07:21:31 -0500

    fixed package boilerplate to use setuptools; added package README

---
+++ README.rst
@@ -0,0 +1,9 @@
+
+Python 3D Bin Packing Library
+=============================
+
+Installation
+------------
+
+`pip install git+https://github.com/USGM/3d-bin-packing-problem#egg=py3dbp_ext`
+
--- setup.py
+++ setup.py
@@ -2,26 +2,8 @@
 Package config script for 3D bin packing library.
 """
 
-import codecs
-from distutils.core import setup
-import os.path
+import setuptools  # noqa
 
-package = "py3dbp_ext"
-
-# see https://packaging.python.org/en/latest/guides/single-sourcing-package-version/
-def read(rel_path):
-    here = os.path.abspath(os.path.dirname(__file__))
-    with codecs.open(os.path.join(here, rel_path), "r") as fp:
-        return fp.read()
-
-
-def get_version(rel_path):
-    for line in read(rel_path).splitlines():
-        if line.startswith("__version__"):
-            delim = '"' if '"' in line else "'"
-            return line.split(delim)[1]
-    else:
-        raise RuntimeError("Unable to find version string.")
-
-
-setup(version=get_version(f"src/{package}/__init__.py"))
+setuptools.setup(
+    setup_requires="setuptools==46.4.0",
+)
