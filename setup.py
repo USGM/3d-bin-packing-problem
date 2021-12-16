730a5ecea2d18c203ffd79f073e6846e85af8c3c
Alexey Pechenkin <alexey74@gmail.com>
2021-12-16 03:03:47 -0500

    BW-compatible pkg version

--- setup.py
+++ setup.py
@@ -2,6 +2,26 @@
 Package config script for 3D bin packing library.
 """
 
+import codecs
 from distutils.core import setup
+import os.path
 
-setup()
+package = "py3dbp_ext"
+
+# see https://packaging.python.org/en/latest/guides/single-sourcing-package-version/
+def read(rel_path):
+    here = os.path.abspath(os.path.dirname(__file__))
+    with codecs.open(os.path.join(here, rel_path), "r") as fp:
+        return fp.read()
+
+
+def get_version(rel_path):
+    for line in read(rel_path).splitlines():
+        if line.startswith("__version__"):
+            delim = '"' if '"' in line else "'"
+            return line.split(delim)[1]
+    else:
+        raise RuntimeError("Unable to find version string.")
+
+
+setup(version=get_version(f"src/{package}/__init__.py"))
